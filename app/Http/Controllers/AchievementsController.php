015463181aaab310316ebd7003e9a55f3e184eef
Nazeeh.Alsaifi <nazeeh.saifi@gmail.com>
2023-10-13 12:23:23 +0400

    initial commit (the downloaded project)

---
+++ app/Http/Controllers/AchievementsController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class AchievementsController extends Controller
+{
+    public function index(User $user)
+    {
+        $unlocked_achievements = [];
+        $next_available_achievements = [];
+        $current_badge = '';
+        $next_badge = '';
+        $remaing_to_unlock_next_badge = 0;
+
+        return response()->json([
+            'unlocked_achievements' => [],
+            'next_available_achievements' => [],
+            'current_badge' => '',
+            'next_badge' => '',
+            'remaing_to_unlock_next_badge' => 0
+        ]);
+    }
+}
