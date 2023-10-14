a85e5b1ddb1f23420483034055d6e80d714675ab
Nazeeh.Alsaifi <nazeeh.saifi@gmail.com>
2023-10-14 18:11:39 +0400

    implemented& tested  AchievementsController index method that return info about user achievements and badges

--- app/Http/Controllers/AchievementsController.php
+++ app/Http/Controllers/AchievementsController.php
@@ -2,25 +2,50 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Achievement;
+use App\Models\Badge;
 use App\Models\User;
-use Illuminate\Http\Request;
 
 class AchievementsController extends Controller
 {
     public function index(User $user)
     {
-        $unlocked_achievements = [];
+        $unlocked_achievements = $user->achievements->pluck('name');
+
+        $current_comments_written_achievement = $user->achievements()
+            ->where('type', 'comments_written')
+            ->orderBy('count_to_reach', 'desc')
+            ->first();
+
+        $current_lessons_watched_achievement = $user->achievements()
+            ->where('type', 'lessons_watched')
+            ->orderBy('count_to_reach', 'desc')
+            ->first();
+
+        $next_comments_written_achievement = Achievement::where('count_to_reach', '>', $current_comments_written_achievement?->count_to_reach ?? 0)
+            ->where('type', 'comments_written')
+            ->first();
+
+        $next_lessons_watched_achievement = Achievement::where('count_to_reach', '>', $current_lessons_watched_achievement?->count_to_reach ?? 0)
+            ->where('type', 'lessons_watched')
+            ->first();
+
         $next_available_achievements = [];
-        $current_badge = '';
-        $next_badge = '';
-        $remaing_to_unlock_next_badge = 0;
+        array_push($next_available_achievements, $next_comments_written_achievement?->name ?? []);
+        array_push($next_available_achievements, $next_lessons_watched_achievement?->name ?? []);
+
+        $current_badge = $user->badges()->latest()->firstOrFail();
+
+        $next_badge = Badge::where('achievements_count', '>', $current_badge->achievements_count)->orderBy('achievements_count')->firstOrFail();
+
+        $remaining_to_unlock_next_badge = ($next_badge?->achievements_count - $current_badge->achievements_count) ?? 0;
 
         return response()->json([
-            'unlocked_achievements' => [],
-            'next_available_achievements' => [],
-            'current_badge' => '',
-            'next_badge' => '',
-            'remaing_to_unlock_next_badge' => 0
+            'unlocked_achievements' => $unlocked_achievements,
+            'next_available_achievements' => $next_available_achievements,
+            'current_badge' => $current_badge->name,
+            'next_badge' => $next_badge->name,
+            'remaining_to_unlock_next_badge' => $remaining_to_unlock_next_badge,
         ]);
     }
 }
--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -2,21 +2,82 @@
 
 namespace Tests\Feature;
 
+use App\Models\Achievement;
+use App\Models\Badge;
 use App\Models\User;
 // use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
 class ExampleTest extends TestCase
 {
+    use RefreshDatabase;
     /**
      * A basic test example.
      */
-    public function test_the_application_returns_a_successful_response(): void
+    public function test_the_application_returns_a_successful_response_and_have_5_keys(): void
     {
         $user = User::factory()->create();
+
+        $response = $this->get("/users/{$user->id}/achievements");
+
+        $response->assertOk()->assertJsonCount(5);
+    }
+
+    public function test_the_application_returns_a_successful_response_and_correct_structure(): void
+    {
+        $user = User::factory()->create();
+
+        $response = $this->get("/users/{$user->id}/achievements");
+
+        $response->assertOk()->assertJsonStructure([
+            'unlocked_achievements' => [],
+            'next_available_achievements' => [],
+            'current_badge',
+            'next_badge',
+            'remaining_to_unlock_next_badge',
+        ]);
+    }
+
+    public function test_the_application_returns_a_successful_response_and_correct_response_data_for_newly_created_user(): void
+    {
+        $user = User::factory()->create();
+        $beginner_badge = Badge::where('name', 'Beginner')->first();
+        $intermediate_badge = Badge::where('name', 'Intermediate')->first();
+        $first_comments_written_achievement = Achievement::where('type', 'comments_written')->where('count_to_reach', 1)->first();
+        $first_lessons_watched_achievement = Achievement::where('type', 'lessons_watched')->where('count_to_reach', 1)->first();
+
+        $response = $this->get("/users/{$user->id}/achievements");
+
+        $response->assertOk()->assertJsonFragment([
+            'unlocked_achievements' => $user->achievements->pluck('name'),
+            'next_available_achievements' => [$first_comments_written_achievement->name, $first_lessons_watched_achievement->name],
+            'current_badge' => $beginner_badge->name,
+            'next_badge' => $intermediate_badge->name,
+            'remaining_to_unlock_next_badge' => $intermediate_badge->achievements_count - $beginner_badge->achievements_count,
+        ]);
+    }
+
+    public function test_the_application_returns_a_successful_response_and_correct_response_data_for_user_with_first_comment_achievement(): void
+    {
+        $user = User::factory()->create();
+        $beginner_badge = Badge::where('name', 'Beginner')->first();
+        $intermediate_badge = Badge::where('name', 'Intermediate')->first();
+        $first_comments_written_achievement = Achievement::where('type', 'comments_written')->where('count_to_reach', 1)->first();
+        $three_comments_written_achievement = Achievement::where('type', 'comments_written')->where('count_to_reach', 3)->first();
+        $first_lessons_watched_achievement = Achievement::where('type', 'lessons_watched')->where('count_to_reach', 1)->first();
+        $user->achievements()->attach($first_comments_written_achievement);
         
         $response = $this->get("/users/{$user->id}/achievements");
 
-        $response->assertStatus(200);
+        $response->assertOk()->assertJsonFragment([
+            'unlocked_achievements' => $user->achievements->pluck('name'),
+            'next_available_achievements' => [$three_comments_written_achievement->name, $first_lessons_watched_achievement->name],
+            'current_badge' => $beginner_badge->name,
+            'next_badge' => $intermediate_badge->name,
+            'remaining_to_unlock_next_badge' => $intermediate_badge->achievements_count - $beginner_badge->achievements_count,
+        ]);
     }
+
+
 }
